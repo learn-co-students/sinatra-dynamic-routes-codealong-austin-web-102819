4fc1ef9f202522cc9ba37bb5f0832071496c89d1
rachfuerst <rach.fuerst@gmail.com>
2019-11-19 11:32:30 -0600

    Done.

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ class App < Sinatra::Base
 
   # This is a sample dynamic route.
   get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+    @user_name = params[:name] # Create the user_name instance variabgle and set it equal to the :name key of the PARAMS hash
+    "Hello #{@user_name}!" # Output the @user_name instance variable to the front end
   end
 
   # Code your final two routes here:
@@ -21,7 +21,13 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @product = params[:num1].to_i * params[:num2].to_i
+    # Create an instance variable @product 
+    # Set @product equal to 
+      # Get the :num1 key from the Params Hash and convert it to an integer (since PARAM hash data is ALWAYS a string)
+      # Get the :num2 key from the Params Hash and convert it to an integer
+      # Multip,y these two values, and set @prodcut equal to the value
+    # Return @product to the screen. 
+    @product = params[:num1].to_i * params[:num2].to_i 
     "#{@product}"
 end
 end
